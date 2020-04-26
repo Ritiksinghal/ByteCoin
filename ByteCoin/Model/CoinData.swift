508620bd8fe5b2bfa4864389c5dd303157dbed5a
Ritik Singhal <singhalritik00@gmail.com>
2020-04-27 04:55:45 +0530

    Add files via upload

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Ritik on 08/02/2020.
+//  Copyright © 2020 Ritik. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
